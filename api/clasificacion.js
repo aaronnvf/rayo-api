1c80e3f497fc50c7d14da8e20a451d8ba43da3ab
aaronnvf <aaronvalero2002@gmail.com>
2026-03-10 09:52:53 +0000

    Update clasificacion.js

--- api/clasificacion.js
+++ api/clasificacion.js
@@ -1,11 +1,24 @@
 export default async function handler(req, res) {
 
-const response = await fetch("https://futbol7amistad.com/api/tournaments/191");
+try {
 
-const data = await response.json();
+const equiposRes = await fetch("https://futbol7amistad.com/api/tournaments/191");
+const equiposData = await equiposRes.json();
+
+const partidosRes = await fetch("https://futbol7amistad.com/api/tournaments/191/filterMatchesByDatesRange");
+const partidosData = await partidosRes.json();
 
 res.setHeader("Access-Control-Allow-Origin", "*");
 
-res.status(200).json(data);
+res.status(200).json({
+teams: equiposData.teams,
+matches: partidosData
+});
+
+} catch (error) {
+
+res.status(500).json({ error: "Error cargando datos" });
+
+}
 
 }
